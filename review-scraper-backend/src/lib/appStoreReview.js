c5ea167f98ccbbc34872c7eb2348d5bd93ded58e
bbyojjok <bbyojjok@gmail.com>
2022-08-30 14:13:29 +0900

    modify backend appStoreReview

--- review-scraper-backend/src/lib/appStoreReview.js
+++ review-scraper-backend/src/lib/appStoreReview.js
@@ -1,32 +1,23 @@
 import axios from 'axios';
-import xml2js from 'xml2js';
-
-const { parseStringPromise } = xml2js;
-
-function replaceAll(str, searchStr, replaceStr) {
-  return str.split(searchStr).join(replaceStr);
-}
 
 const appStoreReview = async ({ id, country, page }) => {
   try {
-    const url = `https://itunes.apple.com/${country}/rss/customerreviews/page=${page}/id=${id}/sortby=mostrecent/xml`;
-    const res = await axios.get(url);
-    const filter = replaceAll(res.data, '<</', '</');
-    const data = await parseStringPromise(replaceAll(filter, '<&lt;&lt;', ''));
-    const entry = data['feed']['entry'];
+    const url = `https://itunes.apple.com/rss/customerreviews/page=${page}/id=${id}/sortby=mostrecent/json?cc=${country}`;
+    const { data } = await axios.get(url);
+    const { entry } = data.feed;
 
     if (entry) {
       return entry.reduce((acc, cur) => {
         const review = {};
-        review.id = cur['id'][0];
+        review.id = cur.id.label;
         review.app = id;
-        review.author = cur['author'][0]['name'][0];
-        review.version = cur['im:version'][0];
-        review.rate = cur['im:rating'][0];
-        review.title = cur['title'][0];
-        review.comment = cur['content'][0]['_'];
-        review.vote = cur['im:voteCount'][0];
-        review.date = cur['updated'][0];
+        review.author = cur.author.name.label;
+        review.version = cur['im:version'].label;
+        review.rate = cur['im:rating'].label;
+        review.title = cur.title.label;
+        review.comment = cur.content.label;
+        review.vote = cur['im:voteCount'].label;
+        review.date = cur.updated.label;
         review.country = country;
         acc.push(review);
         return acc;
