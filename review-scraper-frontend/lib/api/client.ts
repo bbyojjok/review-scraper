2f30066f80bfdada41ec57c097857ce4b88b18dc
bbyojjok <bbyojjok@gmail.com>
2022-09-29 08:58:40 +0900

    axios error test

--- review-scraper-frontend/lib/api/client.ts
+++ review-scraper-frontend/lib/api/client.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const client = axios.create();
 client.defaults.withCredentials = true;
-// client.defaults.baseURL = 'http://localhost:8083';
+client.defaults.baseURL = 'http://localhost:8083';
 
 export default client;
