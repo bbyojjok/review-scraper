1f203e742ec36980e4ff780dc0ccd1343ca6d7ca
bbyojjok <bbyojjok@gmail.com>
2022-09-29 09:25:12 +0900

    axios test

--- review-scraper-frontend/lib/api/client.ts
+++ review-scraper-frontend/lib/api/client.ts
@@ -4,7 +4,7 @@ const client = axios.create();
 client.defaults.withCredentials = true;
 client.defaults.baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://review.stlee.kr'
+    ? 'https://review.stlee.kr'
     : 'http://localhost:8083';
 
 export default client;
