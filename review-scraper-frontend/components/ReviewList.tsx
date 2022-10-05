6a5f1d35684757488ca9d1c418da3e50fb7bd452
bbyojjok <bbyojjok@gmail.com>
2022-10-05 09:02:08 +0900

    modify front reviews component

---
+++ review-scraper-frontend/components/ReviewList.tsx
@@ -0,0 +1,151 @@
+import styled from '@emotion/styled';
+import { MdStar, MdSubdirectoryArrowRight } from 'react-icons/md';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+const ReviewListBlock = styled.div`
+  overflow-y: auto;
+  height: calc(100% - 50px);
+  padding-right: 5px;
+
+  .thumb-vertical {
+    cursor: pointer;
+    border-radius: inherit;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+
+  .review-list {
+    li {
+      margin-top: 15px;
+      padding: 10px;
+      font-size: 12px;
+      background-color: #333;
+      border-radius: 5px;
+
+      &:first-of-type {
+        margin-top: 0;
+      }
+
+      &.center {
+        padding-top: 20px;
+        padding-bottom: 20px;
+        text-align: center;
+      }
+
+      .info {
+        display: flex;
+        flex-wrap: wrap;
+        padding-bottom: 10px;
+
+        .title {
+          width: calc(100% - 80px);
+        }
+
+        .date {
+          text-align: right;
+          width: 80px;
+        }
+
+        .rate {
+          width: 65px;
+        }
+
+        .author {
+          text-align: right;
+          width: calc(100% - 65px);
+          word-break: break-word;
+        }
+      }
+
+      .text {
+        line-height: 1.4;
+        word-break: break-word;
+      }
+
+      .reply-text {
+        margin-top: 15px;
+        padding: 10px;
+        background-color: #444;
+        border-radius: 5px;
+        word-break: break-word;
+
+        p {
+          display: flex;
+          justify-content: space-between;
+          padding-bottom: 5px;
+        }
+      }
+    }
+  }
+`;
+
+type ReviewListProps = {
+  os: string;
+  list: any;
+};
+
+const ReviewList = ({ os, list }: ReviewListProps) => {
+  const isGooglePlay = os === 'GooglePlay';
+
+  return (
+    <ReviewListBlock>
+      <Scrollbars
+        universal
+        renderThumbVertical={(props) => (
+          <div {...props} className="thumb-vertical" />
+        )}
+      >
+        <ul className="review-list">
+          {list.length === 0 && (
+            <li className="center">${os} 리뷰가 없습니다.</li>
+          )}
+          {list.map(({ _id, review }: any) => {
+            const {
+              author,
+              title,
+              comment,
+              rate,
+              userName,
+              text,
+              scoreText,
+              date,
+              replyText,
+              replyDate,
+            } = review;
+            const star = isGooglePlay ? scoreText : rate;
+
+            return (
+              <li key={_id}>
+                <div className="info">
+                  <p className="title">{isGooglePlay || title}</p>
+                  <p className="date">{date}</p>
+                </div>
+                <div className="info">
+                  <p className="rate">
+                    {Array(parseInt(star, 10))
+                      .fill(0)
+                      .map((val, idx) => (
+                        <MdStar key={star + idx} />
+                      ))}
+                  </p>
+                  <p className="author">{isGooglePlay ? userName : author}</p>
+                </div>
+                <div className="text">{isGooglePlay ? text : comment}</div>
+                {isGooglePlay && replyText && (
+                  <div className="reply-text">
+                    <p>
+                      <MdSubdirectoryArrowRight />
+                      {replyDate}
+                    </p>
+                    {replyText}
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </Scrollbars>
+    </ReviewListBlock>
+  );
+};
+
+export default ReviewList;
--- review-scraper-frontend/components/Reviews.tsx
+++ review-scraper-frontend/components/Reviews.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
-import { MdStar, MdSubdirectoryArrowRight } from 'react-icons/md';
 import ReviewTitle from './ReviewTitle';
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import ReviewList from './ReviewList';
 
 const ReviewsBlock = styled.div`
   display: flex;
@@ -25,94 +24,6 @@ const ReviewsBlock = styled.div`
       padding-left: 10px;
     }
   }
-
-  .list-wrap {
-    overflow-y: auto;
-    height: calc(100% - 50px);
-    padding-right: 5px;
-
-    .thumb-vertical {
-      cursor: pointer;
-      border-radius: inherit;
-      background-color: rgba(0, 0, 0, 0.8);
-    }
-
-    /* &::-webkit-scrollbar {
-      width: 6px;
-    }
-    &::-webkit-scrollbar-track {
-      background: none;
-      border: none;
-    }
-    &::-webkit-scrollbar-thumb {
-      background: rgba(0, 0, 0, 0.9);
-      border-radius: 3px;
-    } */
-  }
-
-  .review-list {
-    li {
-      margin-top: 15px;
-      padding: 10px;
-      font-size: 12px;
-      background-color: #333;
-      border-radius: 5px;
-
-      &:first-of-type {
-        margin-top: 0;
-      }
-
-      &.center {
-        padding-top: 20px;
-        padding-bottom: 20px;
-        text-align: center;
-      }
-
-      .info {
-        display: flex;
-        flex-wrap: wrap;
-        padding-bottom: 10px;
-
-        .title {
-          width: calc(100% - 80px);
-        }
-
-        .date {
-          text-align: right;
-          width: 80px;
-        }
-
-        .rate {
-          width: 65px;
-        }
-
-        .author {
-          text-align: right;
-          width: calc(100% - 65px);
-          word-break: break-word;
-        }
-      }
-
-      .text {
-        line-height: 1.4;
-        word-break: break-word;
-      }
-
-      .reply-text {
-        margin-top: 15px;
-        padding: 10px;
-        background-color: #444;
-        border-radius: 5px;
-        word-break: break-word;
-
-        p {
-          display: flex;
-          justify-content: space-between;
-          padding-bottom: 5px;
-        }
-      }
-    }
-  }
 `;
 
 type ReviewsProps = {
@@ -131,58 +42,7 @@ const Reviews = ({ detail, reviews }: ReviewsProps) => {
           url={detail.googlePlay.url}
           length={reviews.googlePlay.length}
         />
-        <div className="list-wrap">
-          <Scrollbars
-            universal
-            renderThumbVertical={(props) => (
-              <div {...props} className="thumb-vertical" />
-            )}
-          >
-            <ul className="review-list">
-              {reviews.googlePlay.length === 0 && (
-                <li className="center">GooglePlay 리뷰가 없습니다.</li>
-              )}
-              {reviews.googlePlay.map(({ _id, name, os, review }: any) => {
-                const {
-                  userName,
-                  text,
-                  scoreText,
-                  date,
-                  replyText,
-                  replyDate,
-                } = review;
-                return (
-                  <li key={_id}>
-                    <div className="info">
-                      <p className="title"></p>
-                      <p className="date">{date}</p>
-                    </div>
-                    <div className="info">
-                      <p className="rate">
-                        {Array(parseInt(scoreText, 10))
-                          .fill(0)
-                          .map((val, idx) => (
-                            <MdStar key={scoreText + idx} />
-                          ))}
-                      </p>
-                      <p className="author">{userName}</p>
-                    </div>
-                    <div className="text">{text}</div>
-                    {replyText && (
-                      <div className="reply-text">
-                        <p>
-                          <MdSubdirectoryArrowRight />
-                          {replyDate}
-                        </p>
-                        {replyText}
-                      </div>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          </Scrollbars>
-        </div>
+        <ReviewList os="GooglePlay" list={reviews.googlePlay} />
       </div>
       <div className="review-wrap">
         <ReviewTitle
@@ -192,37 +52,7 @@ const Reviews = ({ detail, reviews }: ReviewsProps) => {
           url={detail.appStore.url}
           length={reviews.appStore.length}
         />
-        <div className="list-wrap">
-          <Scrollbars universal>
-            <ul className="review-list">
-              {reviews.appStore.length === 0 && (
-                <li className="center">AppStore 리뷰가 없습니다.</li>
-              )}
-              {reviews.appStore.map(({ _id, name, os, review }: any) => {
-                const { author, title, comment, rate, date } = review;
-                return (
-                  <li key={_id}>
-                    <div className="info">
-                      <p className="title">{title}</p>
-                      <p className="date">{date}</p>
-                    </div>
-                    <div className="info">
-                      <p className="rate">
-                        {Array(parseInt(rate, 10))
-                          .fill(0)
-                          .map((val, idx) => (
-                            <MdStar key={rate + idx} />
-                          ))}
-                      </p>
-                      <p className="author">{author}</p>
-                    </div>
-                    <div className="text">{comment}</div>
-                  </li>
-                );
-              })}
-            </ul>
-          </Scrollbars>
-        </div>
+        <ReviewList os="AppStore" list={reviews.appStore} />
       </div>
     </ReviewsBlock>
   );
