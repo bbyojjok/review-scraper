cf068d855c9f777d02ab9ecb58a753248322762d
bbyojjok <bbyojjok@gmail.com>
2023-10-12 10:09:44 +0900

    next config modify7

--- review-scraper-frontend/lib/api/client.ts
+++ review-scraper-frontend/lib/api/client.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 const API_BASEURL = process.env.NEXT_PUBLIC_API_BASEURL;
-console.log(
-  'process.env.NEXT_PUBLIC_API_BASEURL:',
-  process.env.NEXT_PUBLIC_API_BASEURL,
-);
 const client = axios.create();
 client.defaults.withCredentials = true;
 client.defaults.baseURL =
