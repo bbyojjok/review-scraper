74490b943610a5a2a610c8e8d746a07cfd5e69b6
bbyojjok <bbyojjok@gmail.com>
2023-10-12 09:28:05 +0900

    next config modify4

--- review-scraper-frontend/lib/api/client.ts
+++ review-scraper-frontend/lib/api/client.ts
@@ -5,6 +5,8 @@ console.log('production' ? API_URL : 'http://localhost:8083');
 const client = axios.create();
 client.defaults.withCredentials = true;
 client.defaults.baseURL =
-  process.env.NODE_ENV === 'production' ? API_URL : 'http://localhost:8083';
+  process.env.NODE_ENV === 'production'
+    ? 'https://reviewback.stlee.kr'
+    : 'http://localhost:8083';
 
 export default client;
