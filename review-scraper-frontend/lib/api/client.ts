33c45d1ca5cc9de8ec79c15a89cd2aeaf4cbbc3f
bbyojjok <bbyojjok@gmail.com>
2023-10-12 09:38:16 +0900

    next config modify5

--- review-scraper-frontend/lib/api/client.ts
+++ review-scraper-frontend/lib/api/client.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const API_URL = process.env.API_URL;
-console.log('production' ? API_URL : 'http://localhost:8083');
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
+console.log('process.env.GENERATE_SOURCEMAP:', process.env.GENERATE_SOURCEMAP);
+console.log(
+  'process.env.API_URL:',
+  'production' ? API_URL : 'http://localhost:8083',
+);
 const client = axios.create();
 client.defaults.withCredentials = true;
 client.defaults.baseURL =
